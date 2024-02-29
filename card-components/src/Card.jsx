a3070cf3b900a991a1b659b79e63729bf1283a32
YewIthipunkul <139699878+Yewwys@users.noreply.github.com>
2024-02-29 09:53:26 +0700

    Creating Card components
    Look like introduction card that professor in the university use in their webstie

---
+++ card-components/src/Card.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image"src="/image/cv.jpg" alt="profile picture"></img>
+            <h2 className='card-title'>Yew</h2>
+            <p className='card-text'>I'm study at faculty of engineering</p>
+        </div>
+    );
+}
+
+export default Card
